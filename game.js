1998c61f12164ddd68c616c787ef0ed8912f659d
ckelly124 <collink124@gmail.com>
2021-08-23 09:48:28 -0700

    Added new fonts and button styles

--- game.js
+++ game.js
@@ -2,13 +2,13 @@ let playSc=0;
 let compSc=0;
 //Function chooses Rock, Paper, or Scissors for the computer
 function computerPlay(){
-    const rps=["Rock","Paper", "Scissors"]
+    const rps=["Mudkip","Rowlet", "Cyndaquil"]
     return rps[Math.floor(Math.random()*3)]
 }
 //Functions created to prompt for user input on choosing RPS each round. 
 function playerPrompt(){
     let play= prompt("Rock, Paper, or Scissors?");
-    if(play.toLowerCase()==="rock" || play.toLowerCase()==="paper" || play.toLowerCase()==="scissors"){
+    if(play.toLowerCase()==="m" || play.toLowerCase()==="paper" || play.toLowerCase()==="scissors"){
         return play;
     }
     else{
@@ -35,64 +35,41 @@ function round(playerSelection, computerSelection){
     changeText(`Player chose ${playerSelection}. Computer chose ${computerSelection}`);
     let play=playerSelection.toLowerCase();
     let comp=computerSelection.toLowerCase();
-    const opt=["rock","paper", "scissors"];
+    const opt=["mudkip","rowlet", "cyndaquil"];
     if(play===comp){
         scores.textContent= `Player: ${playSc} Rival: ${compSc}`;
         textBox.textContent= "It's a tie! Try again.";
     } else if (play===opt[0] && comp===opt[1]){
         compSc++;
         scores.textContent= `Player: ${playSc} Rival: ${compSc}`;
-        textBox.textContent= "Ha, you lose! Paper beats Rock.";
+        textBox.textContent= "Ha, you lose! Grass-type beats Water-type.";
     }
     else if (play===opt[1] && comp===opt[2]){
         compSc++;
         scores.textContent= `Player: ${playSc} Rival: ${compSc}`;
-        textBox.textContent= "Ha, you lose! Scissors beats Paper.";
+        textBox.textContent= "Ha, you lose! Fire-type beats Grass-type.";
     } 
     else if (play===opt[2] && comp===opt[0]){
         compSc++;
         scores.textContent= `Player: ${playSc} Rival: ${compSc}`;
-        textBox.textContent= "Ha, you lose! Rock beats Scissors.";
+        textBox.textContent= "Ha, you lose! Water-type beats Fire-type.";
     }
     else if (play===opt[0] && comp===opt[2]){
         playSc++;
         scores.textContent= `Player: ${playSc} Rival: ${compSc}`;
-        textBox.textContent= "You win! Rock beats Scissors.";
+        textBox.textContent= "You win! Water-type beats Fire-type.";
     }
     else if (play===opt[1] && comp===opt[0]){
         playSc++;
         scores.textContent= `Player: ${playSc} Rival: ${compSc}`;
-        textBox.textContent= "You win! Paper beats Rock.";
+        textBox.textContent= "You win! Grass-type beats Water-type.";
     }
     else if (play===opt[2] && comp===opt[1]){
         playSc++;
         scores.textContent= `Player: ${playSc} Rival: ${compSc}`;
-        textBox.textContent= "You win! Scissors beats Paper.";
+        textBox.textContent= "You win! Fire-type beats Grass-type.";
     }
     checkScore(playSc,compSc);
-}
-function comPlayTest(){
-    let rockCount=0;
-    let paperCount=0;
-    let scissorCount=0;
-    for(let i=0; i<100; i++){
-        let test=computerPlay()
-        switch(test){
-            case "Rock":
-                rockCount++;
-                break;
-            case "Paper":
-                paperCount++;
-                break;
-            case "Scissors":
-                scissorCount++;
-                break;
-        }
-    }
-    let result= `Results:
-     Rock: ${ rockCount } Paper: ${ paperCount }
-     Scissor: ${ scissorCount}`
-    return result;
 }
 
 const checkScore= function(player, rival){
@@ -134,33 +111,33 @@ function playGame(){
 
     const rockButton = document.createElement('button');
     rockButton.classList.add('button');
+    rockButton.setAttribute('id','water');
     rockButton.textContent="Choose Mudkip!";
-    //rockButton.innerHTML = '<img src="images/Cyndaquil.png" />'
     rpsButtons.appendChild(rockButton);
 
 
     const paperButton = document.createElement('button');
     paperButton.classList.add('button');
-    paperButton.textContent="Choose Rowlett!";
-    //paperButton.innerHTML = '<img src="images/Mudkip.png" />'
+    paperButton.setAttribute('id','grass');
+    paperButton.textContent="Choose Rowlet!";
     rpsButtons.appendChild(paperButton);
 
     const scissorsButton = document.createElement('button');
     scissorsButton.classList.add('button');
+    scissorsButton.setAttribute('id','fire');
     scissorsButton.textContent="Choose Cyndaquil!";
-    //scissorsButton.innerHTML = '<img src="images/Rowlett.png" />'
     rpsButtons.appendChild(scissorsButton);
 
     
 
     rockButton.addEventListener('click', () => {
-        round('rock',computerPlay());
+        round('Mudkip',computerPlay());
     });
     paperButton.addEventListener('click', () => {
-        round('paper',computerPlay());
+        round('Rowlet',computerPlay());
     });
     scissorsButton.addEventListener('click', () => {
-        round('scissors',computerPlay());
+        round('Cyndaquil',computerPlay());
     });
 
 }
